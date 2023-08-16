a2d93c07fef6121e8f1fea01de514803b7294056
Othmane Menad <menad.othmane@gmail.com>
2023-08-16 21:25:47 +0100

    Prisma setup finished and connected with mongodb atlas db

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,8 @@ export default function Home() {
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
           <Tech />
-
           <Travel />
-
           <Other />
-
           <div className="hidden md:block">
             <Subscribe />
           </div>
